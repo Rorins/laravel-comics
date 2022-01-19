80d8cf73339bb53e0522d4c4ebf2951cb9439d24
Aurora Grippaudo <aurora268@hotmail.it>
2022-01-19 17:08:01 +0100

    setted footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,13 +1,15 @@
-<footer>
+<footer class="main-footer">
+<div class="container">
     <button>SIGN-UP NOW!</button>
-
-    <h3>FOLLOW US</h3>
+    
     <ul>
         {{-- asset per reperire immagini, parte da public --}}
+        <li><a href="">FOLLOW US</a></li>
         <li><a href=""><img src="{{asset('images/footer-facebook.png')}}" alt="facebook"></a></li>
         <li><a href=""><img src="{{asset('images/footer-twitter.png')}}" alt="twitter"></a></li>
         <li><a href=""><img src="{{asset('images/footer-youtube.png')}}" alt="youtube"></a></li>
         <li><a href=""><img src="{{asset('images/footer-pinterest.png')}}" alt="pinterest"></a></li>
         <li><a href=""><img src="{{asset('images/footer-periscope.png')}}" alt="periscope"></a></li>
     </ul>
+</div>
 </footer>
