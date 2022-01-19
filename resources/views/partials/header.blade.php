b7c0efc16e7e594017b7455a622da3dc90166f29
Aurora Grippaudo <aurora268@hotmail.it>
2022-01-19 16:36:39 +0100

    header styling

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
-<header>
-    <a href="{{route('home')}}"><img src="{{asset('images/dc-logo.png')}}" alt="DC LOGO"></a>
+<header class="main-header container">
+    <a class="logo" href="{{route('home')}}"><img src="{{asset('images/dc-logo.png')}}" alt="DC LOGO"></a>
     <ul>
         {{-- from web.php name() --}}
         <li><a href="{{route('home')}}">COMICS</a></li>
