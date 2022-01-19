6859f75ed48a49037f9401589a707b7b7a2dce45
Aurora Grippaudo <aurora268@hotmail.it>
2022-01-19 19:56:14 +0100

    Added main comics

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,16 @@
 <header class="main-header container">
-    <a class="logo" href="{{route('home')}}"><img src="{{asset('images/dc-logo.png')}}" alt="DC LOGO"></a>
+    <a class="logo" href="{{route('home')}}">
+        <img src="{{asset('images/dc-logo.png')}}" alt="DC LOGO"></a>
     <ul>
         {{-- from web.php name() --}}
-        <li><a href="{{route('home')}}">COMICS</a></li>
-        <li><a href="{{route('news')}}">NEWS</a></li>
+        <li @if(Request::route()->getName === 'comics') class='active' @endif>
+            <a href="{{route('comics')}}">
+            COMICS</a></li>
+
+        <li @if(Request::route()->getName === 'news') class='active' @endif>
+            <a href="{{route('news')}}">
+            NEWS</a></li>
     </ul>
+
+    {{-- @dump(Request::route()->getName()) --}}
 </header>
