ad37c48177430ecb424df1b244d9fb3ad0fb2162
Aurora Grippaudo <aurora268@hotmail.it>
2022-01-19 16:11:21 +0100

    settings for sass

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- IMPORT SASS STYLES, questi finiranno nella cartella public e nel css --}}
+    {{-- per puntare alla public si usa sempre assets --}}
+    <link rel="stylesheet" href="{{asset('css/app')}}">
     <title>Document</title>
 </head>
 <body>
